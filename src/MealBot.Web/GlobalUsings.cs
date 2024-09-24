0304818af9207c196e4ffa2712c7a2f09b5e9ccb
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-09-24 19:39:15 -0400

    ✨ Added Google authentication

---
+++ src/MealBot.Web/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using MealBot.Web;
+
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
--- src/MealBot.Web/Program.cs
+++ src/MealBot.Web/Program.cs
@@ -1,18 +1,19 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MealBot.Web;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddOidcAuthentication(options =>
 {
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("Local", options.ProviderOptions);
-});
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
 
-await builder.Build().RunAsync();
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+    builder.Services.AddOidcAuthentication(options =>
+    {
+        builder.Configuration.Bind("Local", options.ProviderOptions);
+    });
+}
+
+var app = builder.Build();
+{
+
+}
+
+await app.RunAsync();
