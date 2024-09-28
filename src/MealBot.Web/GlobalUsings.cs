d43bdfda7fedb52c87cedc2dd59d1f9c6b26b50c
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-09-28 06:41:57 -0400

    ✨ Added BrowserStorage

--- src/MealBot.Web/GlobalUsings.cs
+++ src/MealBot.Web/GlobalUsings.cs
@@ -1,4 +1,6 @@
 global using MealBot.Web;
+global using MealBot.Web.Services;
 
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.JSInterop;
