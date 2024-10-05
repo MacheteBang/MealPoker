232ee1127a22dd33c78feb3491dfbbf860405e0f
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-10-05 10:28:31 -0400

    🚚 Updated Auth `health` route

--- src/MealBot.Auth/Globals.cs
+++ src/MealBot.Auth/Globals.cs
@@ -3,8 +3,10 @@ namespace MealBot.Auth;
 public static class Globals
 {
     public const string BaseRoute = "/auth";
+    public const string SignInLocationRoute = $"{BaseRoute}/signin-locations";
+
     public const string SignInGoogleRoute = $"{BaseRoute}/sign-in/google";
     public const string RedirectGoogleRoute = $"{BaseRoute}/redirect/google";
     public const string RefreshTokenRoute = $"{BaseRoute}/refresh-token";
-    public const string HealthRoute = "/auth-health";
+    public const string HealthRoute = $"/health{BaseRoute}";
 }
