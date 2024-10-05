aa4c6e116b4e4fc4002ea248c9d94d527e93c278
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-10-05 17:16:30 -0400

    🎉 Reset `MealBot.Web`

--- src/MealBot.Web/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using MealBot.Web;
-global using MealBot.Web.Services;
-
-global using Microsoft.AspNetCore.Components.Web;
-global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-global using Microsoft.JSInterop;
--- src/MealBot.Web/Program.cs
+++ src/MealBot.Web/Program.cs
@@ -1,22 +1,11 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MealBot.Web;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-{
-    builder.RootComponents.Add<App>("#app");
-    builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-    builder.Services.AddScoped<BrowserStorageService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
-    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-    builder.Services.AddOidcAuthentication(options =>
-    {
-        builder.Configuration.Bind("Local", options.ProviderOptions);
-    });
-}
-
-var app = builder.Build();
-{
-
-}
-
-await app.RunAsync();
+await builder.Build().RunAsync();
