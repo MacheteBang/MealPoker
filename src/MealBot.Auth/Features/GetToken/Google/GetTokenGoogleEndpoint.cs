d430d06865e4d56997d4b8b1f248b4b452ab1d88
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-10-07 08:08:00 -0400

    🏗️ Appended refresh token to the cookie on the google redirect

--- src/MealBot.Auth/Features/GetToken/Google/GetTokenGoogleEndpoint.cs
+++ src/MealBot.Auth/Features/GetToken/Google/GetTokenGoogleEndpoint.cs
@@ -1,10 +1,11 @@
 namespace MealBot.Auth.Features.GetToken.Google;
 
-internal sealed class GetTokenEndpoint : AuthEndpoint
+internal sealed class GetTokenEndpoint() : AuthEndpoint
 {
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapGet(Globals.GetTokenGoogleRoute, async (
+            HttpContext context,
             string authorizationCode,
             string callBackUri,
             ISender sender) =>
@@ -17,13 +18,25 @@ internal sealed class GetTokenEndpoint : AuthEndpoint
             var query = new GetTokenGoogleQuery(authorizationCode, callBackUri);
             var result = await sender.Send(query);
 
-            // TODO: Append the refresh token to the Cookies
+            if (result.IsError)
+            {
+                return Results.Unauthorized();
+            }
+
+            var refreshTokenOptions = context.RequestServices.GetRequiredService<IOptions<RefreshTokenOptions>>();
+
+            var refreshToken = result.Value.RefreshToken;
+            context.Response.Cookies.Append(refreshTokenOptions.Value.CookieName, refreshToken.Value, new()
+            {
+                Secure = true,
+                HttpOnly = true,
+                Path = "/api/token/refresh",
+                SameSite = SameSiteMode.None,
+                Expires = refreshToken.ExpiresAt
+            });
 
             TokenResponse tokenResponse = new(result.Value.AccessToken);
-
-            return result.Match(
-                token => Results.Ok(tokenResponse),
-                _ => Results.Unauthorized());
+            return Results.Ok(tokenResponse);
         });
     }
 }
