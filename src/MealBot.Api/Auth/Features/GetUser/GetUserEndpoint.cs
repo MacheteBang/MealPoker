2f09691271214bb2ddf0ec01e9772458423efb91
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-10-16 10:07:27 -0400

    🚚 Corrected signature of users route

--- src/MealBot.Api/Auth/Features/GetUser/GetUserEndpoint.cs
+++ src/MealBot.Api/Auth/Features/GetUser/GetUserEndpoint.cs
@@ -4,16 +4,22 @@ internal sealed class GetUserEndpoint : MealBotEndpoint
 {
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet(GlobalSettings.RoutePaths.Users, async (HttpContext httpContext, ISender sender) =>
+        app.MapGet($"{GlobalSettings.RoutePaths.Users}/{{userId:Guid}}", async (
+            HttpContext httpContext,
+            ISender sender,
+            Guid userId) =>
         {
-            string? emailAddress = httpContext.User.FindFirst(ClaimTypes.Email)?.Value;
-            if (string.IsNullOrEmpty(emailAddress))
+            if (!Guid.TryParse(httpContext.User.FindFirstValue(ClaimTypes.NameIdentifier), out var claimUserId))
             {
-                // TODO: Should this be a different response?
                 return Results.Unauthorized();
             }
 
-            var query = new GetUserQuery(emailAddress);
+            if (claimUserId != userId)
+            {
+                return Results.Unauthorized();
+            }
+
+            var query = new GetUserQuery(userId);
             var result = await sender.Send(query);
 
             return result.Match(
