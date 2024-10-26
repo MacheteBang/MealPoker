2afe637002a3e33290b2d1417192d3311f4fe4d0
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-10-26 09:59:25 -0400

    📱 Restyled the AddMeal page

--- src/MealBot.Web/Features/Meals/FormModels/MealFormModel.cs
+++ src/MealBot.Web/Features/Meals/FormModels/MealFormModel.cs
@@ -11,7 +11,7 @@ public class MealFormModel
     public class MealPartFormModel
     {
         public MealPartCategory Category { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; } = "";
         public string? Description { get; set; }
         public string? Url { get; set; }
     }
