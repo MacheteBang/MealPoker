d45e436bb6fcd217eca1e00a28cfd354e8fd4b55
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-10-29 07:34:12 -0400

    🔥 Removed `Permission` model (and support)

--- src/MealBot.Api/Auth/Models/Permission.cs
+++
@@ -1,8 +0,0 @@
-namespace MealBot.Api.Auth.Models;
-
-internal sealed class Permission
-{
-    public required Guid UserId { get; init; }
-    public required Guid Subject { get; init; }
-    public required string SubjectType { get; init; }
-}
--- src/MealBot.Api/Database/MealBotDbContext.cs
+++ src/MealBot.Api/Database/MealBotDbContext.cs
@@ -4,16 +4,11 @@ namespace MealBot.Api.Database;
 
 internal sealed class MealBotDbContext(DbContextOptions<MealBotDbContext> options) : DbContext(options)
 {
-    public DbSet<Permission> Permissions { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Meal> Meals { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Permission>()
-            .HasKey(u => new { u.UserId, u.Subject })
-            .HasName("PK_Permissions_UserId_Subject");
-
         modelBuilder.Entity<User>()
             .HasKey(u => u.UserId)
             .HasName("PK_Users_UserId");
