e5a06a84ec1a5f0fbf65427d9adf316daa9fa85c
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-10-31 12:25:23 -0400

    ✨ User can now see all members of their family

--- src/MealBot.Api/Users/Features/GetUserProfileImage/GetUserProfileImageEndpoint.cs
+++ src/MealBot.Api/Users/Features/GetUserProfileImage/GetUserProfileImageEndpoint.cs
@@ -4,6 +4,7 @@ internal sealed class GetUserProfileImageEndpoint : MealBotEndpoint
 {
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
+        // TODO: Allow resizing of profile image at server to reduce bandwidth to the client
         app.MapGet($"{GlobalSettings.RoutePaths.Users}/{{userId:Guid}}{GlobalSettings.RoutePaths.ProfileImages}", async (Guid userId, ISender sender) =>
         {
             var query = new GetUserProfileImageQuery(userId);
