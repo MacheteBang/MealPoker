33ba30ba9e7a2ac0963bf8e47bd78083bd51490c
Jonathan Gregorsky <jonathan.gregorsky@gmail.com>
2024-11-14 13:34:58 -0500

    🐛 Cascade delete user ratings when a meal is deleted

--- src/MealBot.Database/Tables/UserMealRatings.sql
+++ src/MealBot.Database/Tables/UserMealRatings.sql
@@ -6,6 +6,6 @@ CREATE TABLE [dbo].[UserMealRatings]
    [Rating] VARCHAR(16) NOT NULL,
 
    CONSTRAINT [PK_UserMealRatings] PRIMARY KEY ([UserId], [MealId]),
-   CONSTRAINT [FK_UserMealRatings_Meals] FOREIGN KEY ([MealId]) REFERENCES [dbo].[Meals] ([MealId]),
+   CONSTRAINT [FK_UserMealRatings_Meals] FOREIGN KEY ([MealId]) REFERENCES [dbo].[Meals] ([MealId]) ON DELETE CASCADE,
    CONSTRAINT [FK_UserMealRatings_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([UserId])
 )
